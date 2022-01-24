b0ad9402f29e4a8861a14aee87cdd1ee81ea1db9
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2022-01-24 10:10:35 +0100

    quest15

---
+++ src/Controller/ActorController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Actor;
+use App\Entity\Program;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/actor", name="actor_")
+ */
+class ActorController extends AbstractController
+{
+    /**
+     * @Route("/{id}", name="show")
+     * @return Response
+     */
+    public function show(Actor $actor): Response
+    {
+        return $this->render('actor/show.html.twig', [
+            'actor' => $actor,
+        ]);
+    }
+}
